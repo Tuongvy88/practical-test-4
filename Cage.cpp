71999f17c3389de2e04e1b2716678aedeb0ec85e
Tuongvy88 <109933390+Tuongvy88@users.noreply.github.com>
2022-09-07 13:46:01 +0930

    Add files via upload

--- Cage.cpp
+++ Cage.cpp
@@ -1,23 +1,38 @@
 #include "Cage.h"
-
-Cage :: Cage(){
-    id = 0;
-    occupant = "";
+#include<iostream>
+using namespace std;
+//default constructor 
+Cage::Cage()
+{
+id=0;
+name=""; 
 }
- 
-Cage :: Cage(int newNumber, std::string newName){
-    id = newNumber;
-    occupant = newName;
+//parameterized constructor
+Cage::Cage(int newNumber,string newName)
+{
+id=newNumber;
+name=newName; 
 }
-
-std::string Cage ::  get_name(){
-    return occupant;
+//getter methods to return name and id
+string Cage::get_name()
+{
+return name;
 }
-
-int Cage :: get_ID_number(){
-    return id;
+int Cage::get_id_number()
+{
+return id;
 }
-
-Cage :: ~Cage(){
-    
+//setter methods to set name and id
+void Cage::set_name(string newName)
+{
+name=newName;
 }
+void Cage::set_id_number(int newNumber)
+{
+id=newNumber;
+}
+//destructor 
+Cage::~Cage()
+{
+cout<<"object destructed.."<<endl; 
+} 
--- Cage.h
+++ Cage.h
@@ -1,20 +1,23 @@
-// Cage.h
+//preventing multiple inclusion of header files 
 #ifndef CAGE_H
 #define CAGE_H
-
-#include <iostream>
-
+#include<iostream>
+using namespace std;
+//Cage class defnition
 class Cage
 {
-    public:
-        Cage(); 
-        Cage(int newNumber, std::string newName); 
-        std::string get_name(); 
-        int get_ID_number(); 
-        ~Cage();
-    private:
-        int id;
-        std::string occupant;
+//instance variable declaration
+private:
+int id;
+string name;
+//method prototypes 
+public:
+Cage();
+Cage(int,string);
+string get_name();
+int get_id_number();
+void set_name(string);
+void set_id_number(int);
+~Cage(); 
 };
-
 #endif 
